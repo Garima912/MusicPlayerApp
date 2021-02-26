67478a1349eeadf8ceb12dbce21142ecb35f808b
Garima912 <gchaud3@uic.edu>
2021-02-26 02:27:51 -0600

    song thumbnails added and song view created

--- app/src/main/java/com/example/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.musicplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import java.sql.Time;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] thumbnails = new int[]{R.drawable.attention, R.drawable.bad_guy, R.drawable.believer, R.drawable.blankspace,
+            R.drawable.blindinglights, R.drawable.eastside, R.drawable.faded, R.drawable.likemebetter, R.drawable.loseyou,
+            R.drawable.memories, R.drawable.middle, R.drawable.nightchanges, R.drawable.story_of_my_life, R.drawable.willow};
+
+    private ArrayList<String> songTitles;
+    private ArrayList<String>  artistNames;
+    private TextView menuBarTxt;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menuBarTxt = (TextView) findViewById(R.id.menuTxt);
+
+        setUpMenuBar();
+        songTitles =  new ArrayList<>();
+        artistNames = new ArrayList<>();
+        songTitles.addAll(Arrays.asList(getResources().getStringArray(R.array.Song_Titles)));
+        artistNames.addAll(Arrays.asList(getResources().getStringArray(R.array.Artist_Names)));
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        MyAdapter adapter = new MyAdapter(songTitles,artistNames,thumbnails);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        //recyclerView.setLayoutManager(new GridLayoutManager(this,2)); //use this line to see as a grid
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); //use this line to see as a standard vertical list
+
+
+    }
+
+    private void setUpMenuBar() {
+        int currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if(currentTime >= 3 && currentTime < 12){
+            menuBarTxt.setText("Good Morning");
+        }
+        if(currentTime >= 12 && currentTime <= 16){
+            menuBarTxt.setText("Good Afternoon");
+        }
+        if(currentTime > 16 && currentTime <= 20){
+            menuBarTxt.setText("Good Evening");
+        }
+        if(currentTime > 20 && currentTime < 3 ){
+            menuBarTxt.setText("Good Night");
+        }
     }
 }
--- app/src/main/java/com/example/musicplayerapp/MyAdapter.java
+++ app/src/main/java/com/example/musicplayerapp/MyAdapter.java
@@ -1,4 +1,77 @@
 package com.example.musicplayerapp;
 
-public class MyAdapter  {
+import android.content.Context;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private ArrayList<String> titles;
+    private ArrayList<String> artists;
+    private int[] songThumbnails;
+
+
+    public MyAdapter(ArrayList<String> songTitles, ArrayList<String> songArtists, int[] thumbnails ) {
+        this.titles = songTitles;
+        this.artists = songArtists;
+        this.songThumbnails = thumbnails;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View songView = inflater.inflate(R.layout.song_view, parent, false);
+        return new MyViewHolder(songView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.songTitle.setText(titles.get(position));
+        holder.artistName.setText(artists.get(position));
+        holder.songImageView.setImageResource(songThumbnails[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return songThumbnails.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener{
+
+        ImageView songImageView;
+        TextView songTitle;
+        TextView artistName;
+        View itemView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            songImageView = (ImageView) itemView.findViewById(R.id.songImage);
+            songTitle = (TextView) itemView.findViewById(R.id.titleTxt);
+            artistName = (TextView) itemView.findViewById(R.id.artistTxt);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.i("ON_CLICK", "item was clicked !");
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        }
+    }
 }
